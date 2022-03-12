9e86ead820d3113a2dca9c56695f0144aed51279
Niteshkumar152 <nitesh26589@gmail.com>
2022-03-12 18:32:37 +0530

    Layout for Home and Navbar Component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GithubPage from "./pages/GithubPage";
+import Home from "./pages/Home";
+import Navbar from "./components/layout/Navbar";
 import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
     <GithubProvider>
       <Router>
+      <Navbar />
         <Routes>
-          <Route path="/" element={<GithubPage />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/custom-github-page" element={<GithubPage />} />
         </Routes>
       </Router>
     </GithubProvider>
