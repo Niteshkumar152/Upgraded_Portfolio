b47e45adf18e3cf7b17c55577906711cbd027d48
Niteshkumar152 <nitesh26589@gmail.com>
2022-08-15 19:17:35 +0530

    Home Page main component created

---
+++ src/constants/social_links.js
@@ -0,0 +1,25 @@
+import {
+    FaLinkedin,
+    FaGithub,
+    FaGoogle
+  } from "react-icons/fa"
+
+  const data = [
+    {
+      id: 1,
+      icon: <FaLinkedin size={"2.5em"}></FaLinkedin>,
+      url: "https://www.linkedin.com/in/nitesh-kumar-nitj/",
+    },
+    {
+      id: 2,
+      icon: <FaGithub size={"2.5em"}></FaGithub>,
+      url: "https://github.com/Niteshkumar152",
+    },
+    {
+        id: 3,
+        icon: <FaGoogle size={"2.5em"}></FaGoogle>,
+        url: "mailto:nitesh26589@gmail.com",
+      }
+  ]
+  
+  export default data
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,50 @@
+import Typewriter from "typewriter-effect";
+import socialLinks from "../constants/social_links";
+import heroImg from "../components/layout/assets/98636-coding.gif";
+
 function Home() {
-  return <div>Home Component comming soon</div>;
+  return (
+    <div className="grid grid-cols-8 mt-14 md:mt-0">
+      <div className="col-span-8 md:col-span-5 m-auto">
+        <div className="p-2">
+          <h2 className="text-xl md:text-3xl">
+            Hey There <span className="wave">👋</span>
+          </h2>
+          <h1 className="text-5xl md:text-6xl">I'm Nitesh</h1>
+          <h4 className="text-sm md:text-sm">
+            Software Developer at{" "}
+            <span className="text-red-500 font-bold">ORACLE</span> Corporation
+          </h4>
+          <Typewriter
+            options={{
+              strings: ["Welcome to my Portfolio ..."],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        <div className="px-2 py-2 md:mx-0">
+          <button to="/contact" className="btn text-xs md:text-sm">
+            contact me
+          </button>
+        </div>
+        <div>
+          <div className="px-2 py-2 flex">
+            {socialLinks.map((link) => {
+              return (
+                <a href={link.url} key={link.id} className="mr-4">
+                  {link.icon}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="mt-0 col-span-3 hidden md:block bg-white">
+        <img src={heroImg} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
