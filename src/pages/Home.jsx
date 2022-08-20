355d6ece993c7e9024e311cb82a948d0dd4720d7
Niteshkumar152 <nitesh26589@gmail.com>
2022-08-20 22:00:40 +0530

    Some UI enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Typewriter from "typewriter-effect";
 //import socialLinks from "../constants/social_links";
 import heroImg from "../components/layout/assets/98636-coding.gif";
 import heroMobileImg from "../components/layout/assets/hero_mobile_image.png";
+import { FaAddressBook, FaDownload } from "react-icons/fa";
 
 function Home() {
   return (
@@ -11,30 +12,33 @@ function Home() {
           <div className="mobile-hero-image md:hidden">
             <img src={heroMobileImg} alt="hero-mobile-img" />
           </div>
-          <h2 className="text-xl md:text-3xl cursor-default mx-10">
+          <h2 className="text-center text-xl md:text-3xl cursor-default">
             Hey There <span className="wave">👋</span>
           </h2>
           <h1 className="text-5xl md:text-6xl cursor-default">I'm Nitesh</h1>
 
-          <Typewriter
-            options={{
-              strings: ["Welcome to my Portfolio ..."],
-              autoStart: true,
-              loop: true,
-            }}
-          />
+          <div className="text-center text-xl md:text-2xl cursor-default font-sans italic">
+            <Typewriter
+              options={{
+                strings: ["Welcome to my Portfolio ..."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         </div>
         <div className="px-2 py-2 md:mx-0">
           <button
             to="/contact"
             className="btn text-xs md:text-sm hover:bg-purple-400"
           >
-            contact me
+            <FaAddressBook className="mr-2" /> contact me
           </button>
           <button
             to="/contact"
             className="btn text-xs text-black hover:text-white ml-2 md:text-sm bg-white hover:bg-purple-400"
           >
+            <FaDownload className="mr-2" />
             download cv
           </button>
         </div>
