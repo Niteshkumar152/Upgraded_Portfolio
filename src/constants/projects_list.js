fbf40c8e587a3949d05fcdd61ab79c5b518a8df5
Niteshkumar152 <nitesh26589@gmail.com>
2022-08-31 16:58:06 +0530

    Weather image added

--- src/constants/projects_list.js
+++ src/constants/projects_list.js
@@ -10,12 +10,12 @@ const data = [
   },
   {
     id: 1,
-    projectTitle: "Crypto Count",
-    projectDescription: "This Project is a chrome extension that fetches the live prices of various crypto currencies . All the data that is rendered on the chrome extension's front end is fetched via an API.",
-    projectLink: "https://github.com/Niteshkumar152/CryptoCount/tree/master/CryptoCount",
-    buttonData: "See Code",
-    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-1.png",
-    techStack:[{tech:"Chrome-extension",color:"success"},{tech:"API's",color:"primary"},{tech:"Bootstrap",color:"accent"}]
+    projectTitle: "Weather App",
+    projectDescription: "Weather App is a single page application that accepts country name/city name as an input from the user and fetches the details about the weather of that place. The front end is built using bootstrap and API calls are made to OpenWeather API to get the data",
+    projectLink: "https://pacific-sea-70481.herokuapp.com/",
+    buttonData: "See Project",
+    projectImg: "https://github.com/Niteshkumar152/Upgraded_Portfolio/blob/main/src/components/layout/assets/project-4.png",
+    techStack:[{tech:"Bootstrap",color:"info"},{tech:"API's",color:"error"}]
   },
   {
     id: 2,
@@ -23,19 +23,36 @@ const data = [
     projectDescription: "This project is a single page website created using React and the data is fetched by making API calls. The homepage shows various different types of cocktails and also cocktails can be searched based on their names.",
     projectLink: "https://cocktail-site.netlify.app/",
     buttonData: "See Project",
-    projectImg: "https://www.dariawan.com/media/images/tech-spring-boot.width-1024.png",
+    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-2.jpg",
     techStack:[{tech:"React",color:"info"},{tech:"API's",color:"error"},{tech:"Netlify",color:"warning"}]
   },
   {
     id: 3,
+    projectTitle: "BookKeeper",
+    projectDescription: "BookKeeper is a website where user's can login via Gmail and add books for sale along with more details about the book and there contact details. The dashboard page contains a list of all the books that have been added for SALE on the website. All CRUD operation are supported like adding a new book, deleting or editing details about a already added book",
+    projectLink: "https://bookkeepernitj.herokuapp.com/",
+    buttonData: "See Project",
+    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-3.png",
+    techStack:[{tech:"Google Authenticator",color:"info"},{tech:"NodeJS",color:"error"},{tech:"Bootstrap",color:"warning"}]
+  },
+  {
+    id: 4,
     projectTitle: "Crypto Count",
-    projectDescription: "This Project is a chrome extension that fetches the live prices of various crypto currencies . All the data that is rendered on the chrome extension's front end is fetched via a API. This Chrome extension can easily be added to chrome browser.",
+    projectDescription: "This Project is a chrome extension that fetches the live prices of various crypto currencies . All the data that is rendered on the chrome extension's front end is fetched via an API.",
     projectLink: "https://github.com/Niteshkumar152/CryptoCount/tree/master/CryptoCount",
     buttonData: "See Code",
+    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-1.png",
+    techStack:[{tech:"Chrome-extension",color:"success"},{tech:"API's",color:"primary"},{tech:"Bootstrap",color:"accent"}]
+  }/* ,
+  {
+    id: 5,
+    projectTitle: "BlogSite",
+    projectDescription: "BlogSite is a very small scale blogging application. The Frontend is created using HTML,CSS,Bootstrap and the Backend is created using NodeJS. Users can add a blog post and that post gets visible on the Home Page of the Website Also all the CRUD (Create, Read, Update, Delete) operations can be performed on the Blog Posts.",
+    projectLink: "https://github.com/Niteshkumar152/BlogSite",
+    buttonData: "See Code",
     projectImg: "https://image.cnbcfm.com/api/v1/image/106928219-1629130755312-gettyimages-1234311531-sindeyev-notitle210729_np12K.jpeg?v=1649925814&w=740&h=416",
-    techStack:[{tech:"Chrome-extension",color:"success"},{tech:"API's",color:"accent"},{tech:"Bootstrap",color:"primary"}]
-  
-  }
+    techStack:[{tech:"Node JS",color:"success"},{tech:"HTML CSS",color:"accent"},{tech:"Heroku",color:"primary"}]
+  } */
 ];
 
 export default data;
