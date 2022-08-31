5a57b6ca71bfa62b71c6b306ba9cb95cba39461a
Niteshkumar152 <nitesh26589@gmail.com>
2022-08-31 15:46:48 +0530

    Image added

--- src/constants/projects_list.js
+++ src/constants/projects_list.js
@@ -5,7 +5,7 @@ const data = [
     projectDescription: "This project is all about creation of various backend APIs for a blogging application. The APIs are designed in such a way that they support all types of CRUD operation and also include Spring Security and authentication of users based on whether the user is an ADMIN or a NORMAL user.",
     projectLink: "https://github.com/Niteshkumar152/Blog-App-API-s",
     buttonData: "See Code",
-    projectImg: "https://www.dariawan.com/media/images/tech-spring-boot.width-1024.png",
+    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-0.png",
     techStack:[{tech:"Java",color:"info"},{tech:"SpringBoot",color:"error"},{tech:"PostMan",color:"warning"}]
   },
   {
@@ -14,7 +14,7 @@ const data = [
     projectDescription: "This Project is a chrome extension that fetches the live prices of various crypto currencies . All the data that is rendered on the chrome extension's front end is fetched via a API. This Chrome extension can easily be added to chrome browser.",
     projectLink: "https://github.com/Niteshkumar152/CryptoCount/tree/master/CryptoCount",
     buttonData: "See Code",
-    projectImg: "https://image.cnbcfm.com/api/v1/image/106928219-1629130755312-gettyimages-1234311531-sindeyev-notitle210729_np12K.jpeg?v=1649925814&w=740&h=416",
+    projectImg: "https://raw.githubusercontent.com/Niteshkumar152/Upgraded_Portfolio/main/src/components/layout/assets/project-1.png",
     techStack:[{tech:"Chrome-extension",color:"success"},{tech:"API's",color:"primary"},{tech:"Bootstrap",color:"accent"}]
   },
   {
